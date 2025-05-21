fc826d40fc66dd71a5d0c0c78669f477e7002219
Krish-pajiyawala <pajiyawalakrish@gmail.com>
2025-05-21 12:09:55 +0530

    Add files via upload

---
+++ 7-PROJECT_1/project-1.js
@@ -0,0 +1,78 @@
+
+
+// write a javascript program to find the area of traingle.
+
+    // let base = (prompt("Enter the base of traingle:")), height = (prompt("Enter the height of traingle:"));
+    // let area =  0.5 * base * height;
+    // console.log("The area of the triangle is " , area);
+
+
+// write a javascript program to convert temperatures to and from celsius,fahrenhit.
+
+    // let temperature = (prompt("Enter the temperature in celsius:"));
+    // let fahrenheit = (temperature * 9/5) + 32;
+    // console.log("The temperature in fahrenheit is " + fahrenheit);
+    // let celsius = (fahrenheit - 32) * 5/9;
+    // console.log("The temperature in celsius is " , celsius);
+
+
+// write a javascript program to area of rectangle.
+
+    // let length = (prompt("Enter the length of rectangle:")), width = (prompt("Enter the width of rectangle:"));
+    // let area = length * width;
+    // console.log("The area of the rectangle is " , area);
+
+// write a javascript program to found area of circle.
+
+    // let radius = (prompt("Enter the radius of circle:"));
+    // let area = 3.14 * radius * radius;
+    // console.log("The area of the circle is " , area);
+
+// write a javascript program to convert to foot into inches.
+
+    // let feet = (prompt("Enter the length in feet:"));
+    // let inches = feet * 12;
+    // console.log("The length in inches is " , inches);
+
+
+
+// write a javascript program to calculate given formulas.
+
+// 1 ==> a**2 - b** 2 = (a - b) * (a + b)
+
+    // let a =30, b = 20  ,result = (a * a) - (b * b);
+    // console.log("The result of the formula is " , result);
+    // let result2 = (a - b) * (a + b);
+    // console.log("The result of the formula is " , result2);
+    
+// 2 ==> (a-b)*2 = a*2 - 2ab + b*2
+
+    // let a = 5, b = 3;
+    // let result = (a - b) ** 2;
+    // console.log("The result of the formula is " , result);
+    // let result2 = (a ** 2) - (2 * a * b) + (b ** 2);
+    // console.log("The result of the formula is " , result2);
+ 
+// 3 ==> (a+b+c)**2 = a*2 + b*2 + c*2 + 2ab + 2ac + 2bc.
+
+    // let a = 10, b = 20, c = 30;
+    // let result = (a + b + c) ** 2;
+    // console.log("The result of the formula is " , result);
+    // let result2 = a ** 2 + b ** 2 + c ** 2 + (2 * a * b) + (2 * a * c) + (2 * c * b);
+    // console.log("The result of the formula is " , result2);
+
+// 4 ==> (a-b-c)**2 = a*2 + b*2 + c*2 - 2ab - 2ac + 2bc.
+
+    // let a = 10, b = 20, c = 30;
+    // let result = (a - b - c) ** 2;
+    // console.log("The result of the formula is " , result);
+    // let result2 = a ** 2 + b ** 2 + c ** 2 - (2 * a * b) - (2 * a * c) + (2 * c * b);
+    // console.log("The result of the formula is " , result2);
+
+// 5 ==> (a-b)**3 a**3-3a**2b+3ab**2-b**3
+
+// let a = 5, b = 4;
+// let result = (a - b) ** 3;
+// console.log("The result of the formula is " , result);
+// let result2 = a**3 - (3 * a**2 * b) + (3 * a * b**2) - (b**3);
+// console.log("The result of the formula is " , result2);
